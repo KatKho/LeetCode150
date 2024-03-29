18f97f52ca9503179202968134bd7c82cb4c0ff0
Ekaterina Khoroshilova <ek.khorosh@gmail.com>
2024-03-29 11:26:40 -0700

    path sum

---
+++ path-sum.js
@@ -0,0 +1,42 @@
+// Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+// A leaf is a node with no children.
+
+// Example 1:
+// Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+// Output: true
+// Explanation: The root-to-leaf path with the target sum is shown.
+
+// Example 2:
+// Input: root = [1,2,3], targetSum = 5
+// Output: false
+// Explanation: There two root-to-leaf paths in the tree:
+// (1 --> 2): The sum is 3.
+// (1 --> 3): The sum is 4.
+// There is no root-to-leaf path with sum = 5.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, targetSum) {
+    if (!root) return false;
+    
+    if (!root.left && !root.right) {
+        return root.val === targetSum;
+    }
+
+    return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(log n) for a balanced tree and O(n) for an unbalanced tree
